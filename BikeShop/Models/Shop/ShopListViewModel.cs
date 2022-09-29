efacfcd954ef1ae42611b2c224fe2a99db969b18
KeithVanW <104157496+KeithVanW@users.noreply.github.com>
2022-09-29 09:18:56 +0200

    added shopdetail

---
+++ BikeShop/Models/Shop/ShopListViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace BikeShop.Models.Shop
+{
+    public class ShopListViewModel
+    {
+        public IEnumerable<ShopListItem> Items { get; set; }
+        public string BannerUrl { get; private set; }
+
+        public string Title { get; } = "Overview";
+
+        public ShopListViewModel()
+        {
+            Random random = new Random();
+            int value = random.Next(0, bannerImages.Length);
+            BannerUrl = bannerImages[value];
+        }
+
+        private string[] bannerImages = new string[]
+        {
+            "/photos/banner/cliff.jpg",
+            "/photos/banner/fjord.jpg",
+            "/photos/banner/northernlight.jpg",
+            "/photos/banner/road.jpg"
+        };
+    }
+}
